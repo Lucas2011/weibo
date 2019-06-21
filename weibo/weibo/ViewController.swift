abd670b90d06c31c3b0493a65a28ab91b5ddd4ca
Lucas <lucas@Lucass-MacBook-Pro.local>
2019-06-21 12:17:06 -0700

    1

---
+++ weibo/weibo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weibo
+//
+//  Created by Lucas on 6/21/19.
+//  Copyright © 2019 Lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
